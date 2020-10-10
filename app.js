c2aebaa35e2a538aac01ee5027cc682421260180
Matt Dillemuth <mattdillemuth@gmail.com>
2020-10-10 04:30:37 -0400

    Replace vanilla JS with jQuery, update UI design

--- app.js
+++ app.js
@@ -1,35 +1,62 @@
-function newItem() {
-  //javascript
-  //1. Adding a new item to the list of items:
-  let li = document.createElement("li");
-  let inputValue = document.getElementById("input").value;
-  let text = document.createTextNode(inputValue);
-  li.appendChild(text);
+function todoList() {
+  // Create new element for todo list
+  let newTodo = $(`<li></li>`);
 
-  if (inputValue === "") {
-    alert("You must write something!");
-  } else {
-    let list = document.querySelector("#list");
-    list.appendChild(li);
-  }
+  // #1. Create new todo to add from input value
+  (function addTodo() {
+    let input = $("#input");
+    let inputVal = $("#input").val();
+    newTodo.append(inputVal);
 
-  //2. Crossing out an item from the list of items:
-  function crossOut() {
-    li.classList.toggle("strike");
-  }
+    let todoList = $("#list");
+    newTodo.addClass("todo__list--item");
 
-  li.addEventListener("dblclick", crossOut);
+    // Create and append error message & style
+    function addError() {
+      $(".error").addClass("error--active");
+      input.addClass("error__input");
+    }
 
-  //3(i). Adding the delete button "X":
-  let crossOutButton = document.createElement("crossOutButton");
-  crossOutButton.appendChild(document.createTextNode("X"));
-  li.appendChild(crossOutButton);
+    // Remove error message & styling
+    function removeError() {
+      $(".error").removeClass("error--active");
+      input.removeClass("error__input");
+    }
 
-  crossOutButton.addEventListener("click", deleteListItem);
-  //3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
-  function deleteListItem() {
-    li.classList.add("delete");
-  }
-  // 4. Reordering the items:
+    // Validate input and add new todo to todo list
+    if (!inputVal) {
+      addError();
+    } else {
+      removeError();
+      todoList.append(newTodo);
+    }
+  })();
+
+  // #2. Mark off a finished todo with double-click
+  newTodo.dblclick(() => newTodo.toggleClass("finished"));
+
+  // #3. Deleting a todo from the list with close button
+  (function removeTodo() {
+    // Creating and adding the remove button
+    let removeBtn = $("<button></button>").addClass("todo__btn--remove");
+    let removeBtnIcon = $("<i></i>").addClass("fas fa-times");
+    removeBtn.append(removeBtnIcon);
+    newTodo.append(removeBtn);
+
+    // Hides the todo item when 'x' button is clicked
+    removeBtn.click(() => newTodo.hide());
+  })();
+
+  // #4. Reordering the items:
   $("#list").sortable();
 }
+
+// Adds todo when user clicks 'Add' button
+$("#btn").click(todoList);
+
+// Adds todo when hitting 'Enter' from input
+$(":input").keyup((e) => {
+  if (e.key === "Enter") {
+    todoList();
+  }
+});
